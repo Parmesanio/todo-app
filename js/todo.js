761e6776873d4af2965ac78315779687b15e39b0
Sean Parmar <sean.parmar@yahoo.com>
2018-03-30 14:49:36 -0400

    Updated ToDo.js
    Updated with reworked js code

--- js/todo.js
+++ js/todo.js
@@ -1,27 +1,41 @@
 if (typeof(Storage) !== "undefined") {
-  document.getElementById("submit").addEventListener('click', function() {
-    if (document.getElementById("input").value == '') {
-      // Do Nothing
-    }else {
-      localStorage.setItem(localStorage.length,document.getElementById("input").value);
+  // Yippee! We can use localStorage awesomeness
+  //Load Any Previous Todos
+  for (var i = 0; i < localStorage.length; i++) {
+    $("ul").append("<li data-id = " + i +" ><span>X</span> "+ localStorage[localStorage.key(i)] + "</li>");
+  }
+
+  //Strike through ToDo when clicked
+  $("ul").on("click", "li", function() {
+    //Alternate between strike and inherit
+    $(this).toggleClass("completed");
+  });
+
+  //Delete Stored ToDo
+  $("ul").on("click", "span", function(event) {
+    //Prevent other events from firing
+    event.stopPropagation();
+    //Remove from localStorage
+    localStorage.removeItem(localStorage[localStorage.key($(this).parent().attr("data-id"))]);
+    //Remove visible li
+    $(this).parent().fadeOut(function() {
+      $(this).remove();
+    });
+  });
+
+  //Create ToDo
+  $("input[type='text']").on("keypress", function(event) {
+    if (event.which === 13 && $(this).val() !== "") {
+      //Save ToDo
+      localStorage.setItem($(this).val(), $(this).val());
+      //Grab user input
+      var text = $(this).val();
+      //Clear input
+      $(this).val("");
+      //Add new li and append text
+      $("ul").append("<li><span>X</span> "+ text + "</li>");
     }
   });
-  var itemList = document.getElementById("items");
-  for (var i = 0; i < localStorage.length && localStorage.key(i).value !== ''; i++) {
-      itemList.innerHTML += '<p data-itemId='+i+'>'+localStorage.getItem(i)+'</p>';
-  }
-  var pTags = document.querySelectorAll("p");
-  var id;
-  for (var i = 0; i < pTags.length; i++) {
-    pTags[i].addEventListener('click', function() {
-      id = this.getAttribute("data-itemId");
-        localStorage.setItem(id, '');
-        this.parentNode.removeChild(this);
-    });
-  }
-  document.getElementById("clear").addEventListener('click', function() {
-    localStorage.clear();
-  });
 } else {
-    document.getElementById("items").innerHTML = "Local Storage not supported in your browser version.";
+  $("h1").text("Local Storage is not supported.");
 }
